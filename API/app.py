4ea50b020f25eae6caa15dd7356b939416a540bc
Maxime Merlin <67154936+Pierrotpsy@users.noreply.github.com>
2023-03-12 13:15:05 +0100

    Add initial db support

--- API/app.py
+++ API/app.py
@@ -1,14 +1,18 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Api, Resource
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://admin:admin123@db:27017/restaurants'  # Replace with your own URI
 api = Api(app)
+mongo = PyMongo(app)
 
-class HelloWorld(Resource):
+class RestaurantList(Resource):
     def get(self):
-        return {'message': 'Hello, World!'}
+        restaurants = mongo.db.restaurants.find()
+        return {'restaurants': [restaurant for restaurant in restaurants]}
 
-api.add_resource(HelloWorld, '/local')
+api.add_resource(RestaurantList, '/restaurants')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000, debug=True)
